acd87101cdb24348088156ea024241f9996f7e77
amitsharma-007 <justseven@rediffmail.com>
2019-05-03 15:49:17 +0530

    Added my project

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,14 +3,29 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './components/login/login.component';
+import { MainComponent } from './components/main/main.component';
+import { ProfileComponent } from './components/profile/profile.component';
+import { RouterModule, Routes } from '@angular/router';
+
+
+
+const appRoutes:Routes = [
+  {path:'', component:MainComponent},
+  {path:'profile', component:ProfileComponent}
+]
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MainComponent,
+    ProfileComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
